9643e5e2ee742118c40bd2145073c4fef700cf8c
ANVA_DOMEIN\JackG <j.gommeren@anva.nl>
2020-03-05 09:18:43 +0100

    Added dino service and refactored dinolist to accomodate add function to list

--- dinotracker/src/app/dino-add/dino-add.component.ts
+++ dinotracker/src/app/dino-add/dino-add.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from "@angular/forms";
+import {FormGroup, Validators} from "@angular/forms";
 import { FormControl } from "@angular/forms";
+import {validate} from "codelyzer/walkerFactory/walkerFn";
+import {DinolistService} from "../dinolist.service";
 
 @Component({
   selector: 'app-dino-add',
@@ -15,15 +17,16 @@ export class DinoAddComponent implements OnInit {
   }
 
   newDino = new FormGroup({
-    name: new FormControl(''),
-    species: new FormControl(''),
-    level: new FormControl(''),
-    base_hp: new FormControl(''),
-    base_melee: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    species: new FormControl('', Validators.required),
+    level: new FormControl('', Validators.required),
+    base_hp: new FormControl('', Validators.required),
+    base_melee: new FormControl('', Validators.required),
   });
 
   onSubmit(){
     console.dir(this.newDino);
-  }
+    DinolistService.();
+
 
 }
